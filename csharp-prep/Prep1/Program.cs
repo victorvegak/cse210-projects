3ab6b45ec884c8086ac47cc3b8d290a5e585d375
victorvegak <vvega28@outlook.com>
2023-09-17 23:25:09 -0600

    Added comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //Ask user for their name.
         Console.WriteLine("What is your first name? ");
         string firstname = Console.ReadLine();
         Console.WriteLine("What is your last name? ");
